4b86ecb29b9dcf2e236593624a258f1bab39de6f
Mert Tomruk <mert137@gmail.com>
2023-09-30 03:15:17 +0300

    Added some tests, Subtract Numbers Test should fail.

---
+++ app/app/calc.py
@@ -0,0 +1,11 @@
+"""
+Calculator Functions
+"""
+
+def add(x, y):
+    """Add x and y and return the result."""
+    return x + y
+
+def substract(x, y):
+    """Subtract x from y"""
+    return y - x
